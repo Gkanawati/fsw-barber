3960286d5b2005d56cf6e16e9e78bb8dbf228645
gkanawati <gabrielkanawati3@gmail.com>
2024-01-31 22:56:49 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,17 @@
+import { format } from 'date-fns';
 import Header from '../_components/header';
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
   return (
     <>
       <Header />
 
+      <div className='pt-5 px-5'>
+
+        <h2 className='text-xl font-bold'>Olá, Miguel!</h2>
+        <p className='capitalize text-sm'>{format(new Date(), "EEEE',' d 'de' MMMM", { locale: ptBR })}</p>
+      </div>
     </>
   );
 }
