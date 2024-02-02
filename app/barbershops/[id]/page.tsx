98c63c3fe0ff36ed26fd17aebc17bba3043f9870
gkanawati <gabrielkanawati3@gmail.com>
2024-02-01 23:19:21 -0300

    feat: add ServiceItem component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { db } from '@/app/_lib/prisma';
+import { Service } from '@prisma/client';
 import BarbershopInfo from './_components/barbershop-info';
+import ServiceItem from './_components/service-item';
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -17,6 +19,9 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id
+    },
+    include: {
+      services: true
     }
   });
 
@@ -26,7 +31,15 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
   }
 
   return (
-    <BarbershopInfo barbershop={barbershop} />
+    <>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="flex flex-col gap-4 px-5 py-6">
+        {barbershop.services.map((service: Service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </>
   );
 }
 
