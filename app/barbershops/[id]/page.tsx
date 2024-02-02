146bdd42932e64b8646ae34af190bdc802df06e5
gkanawati <gabrielkanawati3@gmail.com>
2024-02-01 22:42:51 -0300

    feat: add BarbershopInfo component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { db } from '@/app/_lib/prisma';
+import BarbershopInfo from './_components/barbershop-info';
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  }
+}
+
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+
+  if (!params.id) {
+    // TODO: Redirect to home page
+    return null
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id
+    }
+  });
+
+  if (!barbershop) {
+    // TODO: Redirect to home page
+    return null
+  }
+
+  return (
+    <BarbershopInfo barbershop={barbershop} />
+  );
+}
+
+export default BarbershopDetailsPage;
